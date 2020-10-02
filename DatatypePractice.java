10efd14f8382993c7d006b4bf616735b947b8328
kefournier <kfournier@hopkinton.k12.ma.us>
2020-10-01 22:03:32 -0400

    first commit

---
+++ DatatypePractice.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class DatatypePractice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DatatypePractice
+{
+    public static void main(String[] args){
+        
+        String name = "Madeleine";
+        int age = 11;
+        int firstTestScore = 98;
+        int secondTestScore = 93;
+        int thirdTestScore = 93;
+        double average = (double)(firstTestScore + secondTestScore + thirdTestScore)/3;
+    
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+        System.out.println("The average of my test scores for this class is: " + average);
+        
+    }
+    
+}
